c08c5721cebce653c7a5ffa4b13e68f6ac29ee08
unknown <20007790@H03-49-17-HPE.glasgowclyde.ac.uk>
2015-10-09 15:43:03 +0100

    added some files

---
+++ Variables/src/variables/variables.java
@@ -0,0 +1,34 @@
+package variables;
+
+public class variables {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int numberOfStudents;
+		String name;
+		double balance;
+		boolean isFinished;
+		char firstInitial, lastInitial;
+		
+		numberOfStudents=21;
+		name="David";
+		balance=102.50;
+		isFinished=true;
+		firstInitial='J';
+		lastInitial='W';
+		System.out.print(name + " ");
+		System.out.println(firstInitial + "" + lastInitial + " ");
+		
+		double rate, interest, principal;
+		rate=0.7;
+		principal=300;
+		interest=rate * principal;
+		System.out.print(interest);
+		
+		
+		
+	}
+
+	
+	
+}
